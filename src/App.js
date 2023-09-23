c4d49c74d41087afd2061560dc69a9ed20faf54c
JA9901 <jareyes999@gmail.com>
2023-09-23 16:08:00 -0400

    redid this project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav.jsx";
+import Footer from "./components/Footer.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
+import BookInfo from "./pages/BookInfo.jsx";
+import { books } from "./data.js";
+import ScrollToTop from "./components/ScrollToTop.jsx";
+import Cart from "./pages/Cart.jsx";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) => {
+        if (item.id === book.id) {
+          return {
+            ...item,
+            quantity: parseFloat(quantity),
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
+  function removeFromCart(item) {
+    setCart(cart.filter((book) => book.id !== item.id));
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach((item) => {
+      counter += item.quantity;
+    });
+    return counter;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ScrollToTop />
+        <Nav numberOfItems={numberOfItems()} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books}
+          addToCart={addToCart} cart={cart} />} />
+          <Route path="/cart" element={<Cart books={books} cart={cart}
+          changeQuantity={changeQuantity} removeFromCart={removeFromCart} />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
